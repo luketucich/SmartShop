d9b4d4edc792475eed01697b6a86505cbec24ac1
luketucich <luketucich@gmail.com>
2025-10-14 18:34:02 -0400

    init: set up project with server and client directories, and app.js Hello World.

---
+++ client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by Luke on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
