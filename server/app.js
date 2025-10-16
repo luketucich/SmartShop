02e35eaf3421ce6db1c903f8b4725fb9a3067ae6
luketucich <luketucich@gmail.com>
2025-10-16 13:13:23 -0400

    feat(db): User model username must be unique and not null, password must be not null.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,21 @@
 const express = require(`express`);
+const models = require(`./models`);
 const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  // Create a new user in the database
+  const newUser = models.User.create({
+    username: username,
+    password: password,
+  });
+
+  res.status(201).json({ message: "User registered successfully" });
+});
 
 app.listen(8080, () => {
   console.log(`Server is running on http://localhost:8080`);
